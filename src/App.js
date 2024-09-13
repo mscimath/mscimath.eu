d1f18f1c520d8e2e2a90719fdecd8d12005f6e76
mscimath <madejscimath@gmail.com>
2024-09-13 11:01:05 +0200

    router fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import JavaScript from './pages/JavaScript';
 import Physics from './pages/Physics';
